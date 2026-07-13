46936bc7856ce919f27962ce06339e49f93be26b
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-07-13 19:29:33 +0530

    commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,10 +11,10 @@ export default function AppHeader() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#aboutus">About Us</Nav.Link>
+            <Nav.Link href="#about">About Us</Nav.Link>
             <Nav.Link href="#services">Services</Nav.Link>
             <Nav.Link href="#works">Works</Nav.Link>
-            <Nav.Link href="#team">Team</Nav.Link>
+            <Nav.Link href="#teams">Team</Nav.Link>
             <Nav.Link href="#testimonials">Testimonials</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <Nav.Link href="#blog">Blog</Nav.Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
